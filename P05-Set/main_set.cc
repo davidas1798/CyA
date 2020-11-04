f5ae0dcb0d7678b72736ad029cc22921f2aac627
David <davidas98@hotmail.com>
2020-11-04 13:24:22 +0000

    Solo faltan operaciones

--- P05-Set/main_set.cc
+++ P05-Set/main_set.cc
@@ -7,29 +7,72 @@
 
 
 #include <iostream>
+#include <ctype.h>
+#include <string>
+#include <fstream>
 #include "set.h"
 
 using namespace std;
 
-int main() {
-  Set set1, set2;
-  int element;
+int main(int argc, char* argv[]) {
   
-  cin >> element;
-  while(element != 0) {
-    set1.Insert(element);
-    cin >> element;
-  }
-
-  cin >> element;
-  while(element != 0) {
-    set2.Insert(element);
-    cin >> element;
+  if(arv[1] == "--help") {
+    cout << "Calculadora de conjuntos:" << endl;
+    cout << "Este programa realiza operaciones con conjuntos, ";
+    cout << "introduzca un fichero de entrada con el formato {elemento1, elemento2...} [operación] {elemento1, elemento2...} ";
+    cout << "y se realizarán dichas operaciones imprimiendose en un fichero de salida" << endl;
   }
   
-  Set result;
-  result = set1 - set2;
-  cout << result << endl;
+  else if(argc != 3) {
+    cerr << "Número de parámetros incorrecto" << endl;
+    cerr << "Usage: ./set_calculator inputfile.txt outputfile.txt" << endl;
+    return 2;
+  }
+  
+  else {
+    ifstream input_file(argv[1]);
+    string line, number;
+    int int_number = 0;
+    bool suma, resta, multiplicacion, complemento;
+    Set* new_set;
+    vector<Set> sets;
+    vector<int> operaciones;
+    
+    while(getline(input_file, line)) {
+      for(int i = 0; i < line.size(); i++) {
+        if(line[i] == '{') {
+          new_set = new Set;
+        }
 
-  return 0;
+        else if(line[i] == '}' || line[i] == ',') {
+          int_number = stoi(number);
+          new_set->Insert(int_number);
+          number.clear();
+        }
+
+        else if(line[i] == '+') {
+          operaciones.push_back(1);
+        }
+        else if(line[i] == '-') {
+          operaciones.push_back(2);
+        }
+        else if(line[i] == '*') {
+          operaciones.push_back(3)
+        }
+        else if(line[i] == '!') {
+          operaciones.push_back(0);
+        }
+
+        else {
+          if(isdigit(line[i]))
+            number.push_back(line[i]);
+        }
+      }
+    }
+
+    for(int i = 0; i < sets.size(); i++)
+      cout << sets[i] << endl;
+
+    return 0;
+  }
 }
